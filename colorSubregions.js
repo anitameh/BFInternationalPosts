22add18d808eb2bc5ff102c18f1a27444755eba4
Anita Mehrotra <anitamehrotra@AnitaMehrotra-MBP.local>
2014-11-05 09:14:03 -0500

    slide bar works, incl files for reformatting, csv to json

--- colorSubregions.js
+++ colorSubregions.js
@@ -3,10 +3,13 @@
  * @date: November 3, 2014
  */
 
-
+// set up svg and map params
 var width = 960,
 	height = 500,
 	margin = 20;
+
+var attributeArray = [],
+    currentAttribute = 0;
 
 var projection = d3.geo.kavrayskiy7(),
     graticule = d3.geo.graticule();
@@ -18,11 +21,6 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-// svg.append("rect")
-//     .attr("width", "100%")
-//     .attr("height", "100%")
-//     .attr("fill", "black");
-
 svg.append("path")
     .datum(graticule)
     .attr("class", "graticule")
@@ -33,8 +31,7 @@ svg.append("path")
     .attr("class", "graticule outline")
     .attr("d", path);
 
-var pvData = [];
-
+// color scale
 var colorScale = [];
 colorScale["en"] = "Indigo";
 colorScale["es"] = "Crimson";
@@ -47,60 +44,75 @@ var color = d3.scale.ordinal()
 	.range(["Indigo", "Crimson", "Gold", "ForestGreen", "Blue"]);
 
 // load data
+var myData = [];
 queue()
     .defer(d3.json, "data/world-50m.json")
-    .defer(d3.csv, "data/post-data-week37.csv", function(d) { 
-        pvData[d.id] = d; 
+    .defer(d3.csv, "data/aggregated-data.csv", function(d) { 
+        myData[d.id] = d; 
     })
+    .defer(d3.csv, "data/aggregated-data.csv")
     .await(ready);
 
 function ready(error, world) {
 
-	// map
+	// draw map
 	var countries = topojson.feature(world, world.objects.countries).features;
-
-	console.log(pvData);
 
     svg.selectAll(".country")
       .data(countries)
     .enter().insert("path", ".graticule")
       .attr("class", "country")
       .attr("d", path)
-      .style("fill", function(d) {
-            if (typeof( pvData[d.id] ) != "undefined") {
-                return d.color = colorScale[ pvData[d.id].Language ];
-            }
-        })
-      .style("opacity", "0.8");
-
+      .style("fill", function(d, i) {
+        if (typeof( myData[parseInt(d.id)] ) != "undefined") {
+            return d.color = colorScale[ myData[parseInt(d.id)].Language ];
+        }
+      })
+      .style("opacity", "0.8");    
 
     // add legend
-    var legend = d3.select("body").append("svg")
-    		.attr("class", "legend")
-			.attr("width", 150)
-			.attr("height", 150)
-    	.selectAll("g")
-    		.data(color.domain().slice().reverse())
-    	.enter().append("g")
-    		.attr("transform", function(d, i) { return "translate(30," + (i*20) + ")"; });
+    drawLegend();
 
-    legend.append("rect")
-    	.attr("width", 18)
-    	.attr("height", 18)
-    	.style("fill", color)
-    	.style("opacity", "0.7");
+    // make slider
+    var pvMax = 100, 
+        pvMin = 0;
 
-    legend.append("text")
-    	.attr("x", 24)
-    	.attr("y", 9)
-    	.attr("dy", ".35em")
-    	.text(function(d) { return d; })
-    		.attr("font-family", "Tahoma")
-    		.attr("font-size", "10");
-
+    var slider = d3.select("#slider")
+        .append("input")
+            .attr("class", "ui-slider-handle")
+            .attr("type", "range")
+            .attr("max", pvMax)
+            .attr("min", pvMin)
+            .style("width", "400px")
+            .property("value", pvMin);
 }
 
 
+
+function drawLegend() {
+
+    var legend = svg.selectAll(".legend")
+            .data(color.domain().slice().reverse())
+        .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(0," + (i*20) + ")"; });
+
+    legend.append("rect")
+        .attr("x", 18)
+        .attr("y", 380)
+        .attr("width", 18)
+        .attr("height", 18)
+        .style("fill", color)
+        .style("opacity", "0.7");
+
+    legend.append("text")
+        .attr("x", 40)
+        .attr("y", 389)
+        .attr("dy", ".35em")
+        .text(function(d) { return d; })
+            .attr("font-family", "Tahoma")
+            .attr("font-size", "10");
+}
 
 
 
