368c82a1a3c2c55d0d52cf9545ad4edae25d62b2
Anita Mehrotra <anitamehrotra@AnitaMehrotra-MBP.local>
2014-11-28 16:33:08 -0500

    updated gitignore

--- cityBubbles2.js
+++ cityBubbles2.js
@@ -23,6 +23,13 @@ var SLIDERWIDTH = 500,
     GLOBALMIN = 0,
     GLOBALMAX = 12941;
 
+
+// color scale
+        var languageColors = d3.scale.linear()
+              .domain(["English", "Espanol", "French", "Portuguese", "Deutsche"])
+              .range(["#67a9cf", "green", "yellow", "red", "purple"]);
+
+
 // load and display the World
 queue()
     .defer(d3.json, "new-data/world-50m.json")
@@ -31,10 +38,9 @@ queue()
     .await(ready);
 
 
+
 // create viz
 function ready(error, world, PV, cities) {
-
-
 
 
     // initialize variables
@@ -43,7 +49,7 @@ function ready(error, world, PV, cities) {
 
 
 
-    // scales
+    // size scale
     var sizeScale = d3.scale.linear()
         .domain([GLOBALMIN, GLOBALMAX])
         .range([0, 100]);
@@ -68,37 +74,37 @@ function ready(error, world, PV, cities) {
 
     // console.log(pageviews[0]);
     var currentDate = 0;
-    var currentData = (PV[ currentDate ].info)[0].pageviews; // 0 => we are assuming the post was first written in english
+    var englishData = (PV[ currentDate ].info)[0].pageviews; // 0 => we are assuming the post was first written in english
 
-    var bubbles = svg.selectAll("circle")
-        .data( currentData )
+    var englishBubbles = svg.selectAll("circle")
+        .data( englishData )
         .enter().append("circle")
         .attr("cx", function(d) { return projection([d.LON, d.LAT])[0]; })
         .attr("cy", function(d) { return projection([d.LON, d.LAT])[1]; })
         .attr("r", function(d) { return sizeScale( d.pv ); })
-        .attr("class", "circle");
+        .attr("class", "englishCircle");
 
 
 
     // hover labels
-    bubbles
-        .on("mouseover", function(d) {
-          div.transition()
-              .duration(200)
-              .style("opacity", 0.85);
-          div.html( function() { 
-              return "<strong>" + d.city + "</strong>" + "<br> <font size='1'>" + 
-                d.pv + " pageviews </font>" +
-                "<br> <font size='1'><font color='grey'> DAY 1 </font></font>"; 
-            })
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-          div.transition()
-              .duration(350)
-              .style("opacity", 0);
-        });
+    // englishBubbles
+    //     .on("mouseover", function(d) {
+    //       div.transition()
+    //           .duration(200)
+    //           .style("opacity", 0.85);
+    //       div.html( function() { 
+    //           return "<strong>" + d.city + "</strong>" + "<br> <font size='1'>" + 
+    //             d.pv + " pageviews </font>" +
+    //             "<br> <font size='1'><font color='grey'> DAY 1 </font></font>"; 
+    //         })
+    //         .style("left", (d3.event.pageX) + "px")
+    //         .style("top", (d3.event.pageY - 28) + "px");
+    //     })
+    //     .on("mouseout", function(d) {
+    //       div.transition()
+    //           .duration(350)
+    //           .style("opacity", 0);
+    //     });
 
 
 
@@ -126,71 +132,75 @@ function ready(error, world, PV, cities) {
 
 
     // update slider and bubbles (inherited from jQuery UI slider)
-    // function update() {
+    function update() {
 
-    //     // update slider value
-    //     var currently = myMin;
-    //     if (currently === myMax) {
-    //       return false;
-    //     }
+        // update slider value
+        var currentDate = myMin;
+        if (currentDate === myMax) {
+          return false;
+        }
 
-    //     slider.property("value", myMin); 
+        slider.property("value", myMin); 
+        englishData = (PV[ currentDate ].info)[0].pageviews;
+        // console.log( englishData[0] );
+        
 
-    //     // get pageviews info
-    //     var currentdate = pageviews[currently].week;
-    //     var currentPvs = [];
-    //     pageviews.forEach(function(d) {
-    //         if (d.week == currentdate) {
-    //             pvs = (d.info[0]).pageviews;
-    //             pvs.forEach(function(dd) {
-    //                 currentPvs[dd.city] = dd.pv;
-    //             });
-    //         }
-    //     });
-
-    //     // update bubble size
-    //     d3.selectAll(".circle").transition()
-    //         .duration(200)
-    //         .attr("r", function(d, i) {
-    //             return sizeScale( currentPvs[d.City] );
-    //         });
-    //         // .attr("fill", function() {
-    //         //   return colorScale( )
-    //         // });
-
-
-    //     // update hover tools
-    //     bubbles
-    //         .on("mouseover", function(d) {
-    //           div.transition()
-    //               .duration(200)
-    //               .style("opacity", 0.85);
-    //           div.html( function() { 
-    //               return "<strong>" + d.City + "</strong>" + "<br> <font size='1'>" + 
-    //                 currentPvs[d.City] + " pageviews </font>" +
-    //                 "<br> <font size='1'><font color='grey'> DAY " + String(currently) + "</font></font>"; 
-    //             })
-    //             .style("left", (d3.event.pageX) + "px")
-    //             .style("top", (d3.event.pageY - 28) + "px");
-    //         })
-    //         .on("mouseout", function(d) {
-    //           div.transition()
-    //               .duration(350)
-    //               .style("opacity", 0);
-    //         });
-
-    //     myMin++;
+        // update bubble size and colors
+        d3.selectAll(".circle").transition()
+            .duration(200)
+            .attr("r", function(d,i) { 
+                // console.log(d);
+                return sizeScale( englishData[i].pv ); 
+            });
+        
+        // english
+        // var englishData = (PV[ currentDate ].info)[0].pageviews;
+        // var bubbles = svg.selectAll("circle")
+        //     .data( currentData )
+        //     .enter().append("circle")
+        //     .attr("cx", function(d) { return projection([d.LON, d.LAT])[0]; })
+        //     .attr("cy", function(d) { return projection([d.LON, d.LAT])[1]; })
+        //     .attr("r", function(d) { return sizeScale( d.pv ); })
+        //     .attr("class", "circle")
+        //     .attr("fill", languageColors( "English" ));
 
 
 
-    // }
+        // hover labels
+        // bubbles
+        //     .on("mouseover", function(d) {
+        //       div.transition()
+        //           .duration(200)
+        //           .style("opacity", 0.85);
+        //       div.html( function() { 
+        //           return "<strong>" + d.city + "</strong>" + "<br> <font size='1'>" + 
+        //             d.pv + " pageviews </font>" +
+        //             "<br> <font size='1'><font color='grey'> DAY 1 </font></font>"; 
+        //         })
+        //         .style("left", (d3.event.pageX) + "px")
+        //         .style("top", (d3.event.pageY - 28) + "px");
+        //     })
+        //     .on("mouseout", function(d) {
+        //       div.transition()
+        //           .duration(350)
+        //           .style("opacity", 0);
+        //     });
 
-    // // use this to activate update() (also inherited from jQuery slider)
-    // setInterval( function() {
-    //     if (myMin <= myMax) {
-    //         update();
-    //     }
-    // }, 400);
+
+
+        // increment
+        myMin++;
+
+
+
+    }
+
+    // use this to activate update() (also inherited from jQuery slider)
+    setInterval( function() {
+        if (myMin <= myMax) {
+            update();
+        }
+    }, 400);
 
     
 }
